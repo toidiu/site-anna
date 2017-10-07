ae2f8aa410723fc6b1cdaa8e838eca23973f2774
toidiu <toidiu@protonmail.com>
2017-10-07 15:42:52 -0400

    called the mta endpoint and not using global store for info

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,36 @@
 <template>
   <section class="section">
     <div>
+
+  {{ timestamp }}
+  <ul v-if="lines && lines.length">
+    <li v-for="line of lines">
+      <p><strong>{{line.name}}</strong></p>
+    </li>
+  </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors">
+      {{error.message}}
+    </li>
+  </ul>
+
       <h1 class="title">
         mta-status
       </h1>
-      <h2 class="subtitle">
-        blo
-        {{ $store.state.mta.timestamp }}
-        </br>
-        bla
-        {{ $store.state.counter }}
-        </br>
-        blu
-        {{ timestamp }}
-      </h2>
+      {{ $store.state.counter }}
       <div class="links">
         <a @click="$store.commit('increment')"  class="button is-primary">Count</a>
       </div>
+
+      <h2 class="subtitle">
+        </br>
+        {{ $store.state.mta.timestamp }}
+        {{ mydata }}
+        </br>
+        </br>
+      </h2>
+
     </div>
   </section>
 </template>
@@ -26,22 +40,44 @@ import axios from 'axios'
 
 export default {
   data () {
-    return { timestamp: 'default' }
-  },
-  components: {
+    return {
+      timestamp: '',
+      lines: [],
+      errors: []
+    }
   },
   asyncData (context) {
-    return { timestamp: 'now' }
-  },
-  async fetch ({ store, params }) {
-    try {
-      let { data } = await axios.get('http://localhost:4000')
-      store.commit('setMta', data)
-    } catch (e) {
-      this.errors.push(e)
+    return {
+      mydata: 2
     }
+  },
+  created () {
+    // axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    axios.get(`http://localhost:4000`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.lines = response.data.lines
+        this.timestamp = response.data.timestamp
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
   }
 }
+// async fetch ({ store, params }) {
+//  try {
+//    let { data } = await axios.get('http://localhost:4000')
+//    this.subLines = data.lines
+//    store.commit('setMta', data)
+//    this.mydata = data
+//  } catch (e) {
+//    this.errors.push(e)
+//  }
+
+//  return {
+//    mydata: 33
+//  }
+// }
 </script>
 
 <style>
